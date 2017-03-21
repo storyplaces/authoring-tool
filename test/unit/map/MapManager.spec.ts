d9f66f2ca782955b011c0b9346575001b78f5a5a
Kevin <kevin@algn.uk>
2017-03-21 11:36:06 +0000

    Updates to tests for the map manager

--- test/unit/map/MapManager.spec.ts
+++ test/unit/map/MapManager.spec.ts
@@ -40,7 +40,7 @@ import {MapControlInterface} from "../../../src/resources/mapping/interfaces/Map
 import {MapLayerInterface} from "../../../src/resources/mapping/interfaces/MapLayerInterface";
 import {LocationInformation} from "../../../src/resources/gps/LocationInformation";
 import {BindingEngine, Container} from "aurelia-framework";
-import {EventAggregator} from "aurelia-event-aggregator";
+import {EventAggregator, Subscription} from "aurelia-event-aggregator";
 import {MapMapLayer} from "../../../src/resources/map/layers/MapMapLayer";
 import {LocationManager, LocationSource} from "../../../src/resources/gps/LocationManager";
 import {CurrentMapLocation} from "../../../src/resources/map/CurrentMapLocation";
@@ -49,8 +49,25 @@ import {RecenterControl} from "../../../src/resources/map/controls/RecenterContr
 import {MapManager} from "../../../src/resources/map/MapManager";
 import {LocationUpdateFromMapEvent} from "../../../src/resources/events/LocationUpdateFromMapEvent";
 import {RequestCurrentLocationEvent} from "../../../src/resources/events/RequestCurrentLocationEvent";
+import {RequestPinDropEvent} from "../../../src/resources/events/RequestPinDropEvent";
+import {CancelPinDropEvent} from "../../../src/resources/events/CancelPinDropEvent";
 
 describe("MapManager", () => {
+
+    beforeEach(() => {
+        mapCore = new MockMapCore() as any;
+        baseLayer = new MockMapLayer() as MapMapLayer;
+        userLocation = new MockLocationManager() as LocationManager;
+        mapLocation = new MockLocation() as CurrentMapLocation;
+        currentLocationMarker = new MockCurrentLocationMarker() as CurrentLocationMarker;
+        recenterControl = new MockRecenterControl() as RecenterControl;
+        eventAggregator = new EventAggregator();
+        locationUpdateFromMapEventFactory = (latitude: number, longitude: number) => new LocationUpdateFromMapEvent(latitude, longitude);
+
+        mapManager = new MapManager(bindingEngine, mapCore as any, baseLayer, userLocation, mapLocation, currentLocationMarker, recenterControl, eventAggregator, locationUpdateFromMapEventFactory);
+        element = document.createElement("div");
+    });
+
     describe("method attach", () => {
         it("attaches the mapcore to the passed map element", () => {
             spyOn(mapCore, 'attachTo');
@@ -440,50 +457,196 @@ describe("MapManager", () => {
 
     describe("event handler", () => {
 
-        it("subscribes to the RequestCurrentLocationEvent when attach is called", (done) => {
-            spyOn(eventAggregator, 'subscribe').and.callFake((event, callback: Function) => {
-                expect(event).toEqual(RequestCurrentLocationEvent);
-                done();
+        describe("subscription", () => {
+            it("subscribes to RequestCurrentLocationEvent when attach is called", () => {
+                spyOn(eventAggregator, 'subscribe');
+                mapManager.attach(element);
+
+                expect(eventAggregator.subscribe).toHaveBeenCalledWith(RequestCurrentLocationEvent, jasmine.anything());
             });
 
-            mapManager.attach(element);
+            it("subscribes to pageListItemSelected when attach is called", () => {
+                spyOn(eventAggregator, 'subscribe');
+                mapManager.attach(element);
+
+                expect(eventAggregator.subscribe).toHaveBeenCalledWith('pageListItemSelected', jasmine.anything());
+            });
+
+            it("subscribes to RequestPinDropEvent when attach is called", () => {
+                spyOn(eventAggregator, 'subscribe');
+                mapManager.attach(element);
+
+                expect(eventAggregator.subscribe).toHaveBeenCalledWith(RequestPinDropEvent, jasmine.anything());
+            });
+
+            it("subscribes to CancelPinDropEvent when attach is called", () => {
+                spyOn(eventAggregator, 'subscribe');
+                mapManager.attach(element);
+
+                expect(eventAggregator.subscribe).toHaveBeenCalledWith(CancelPinDropEvent, jasmine.anything());
+            });
         });
 
-        it("will unsubscribe from the RequestCurrentLocationEvent when detach is called", () => {
-            let subscription = {
-                dispose: function () {
+        describe("un-subsription", () => {
+            let subscription;
+            let eventToListenFor;
+
+            beforeEach(() => {
+                subscription = {
+                    dispose: function () {
+                    }
+                };
+
+                spyOn(eventAggregator, 'subscribe').and.callFake(passedEvent => {
+                    if (passedEvent == eventToListenFor) {
+                        return subscription;
+                    }
+
+                    return undefined;
+                });
+
+                spyOn(subscription, 'dispose').and.callThrough();
+            });
+
+            it("will unsubscribe from the RequestCurrentLocationEvent when detach is called", () => {
+                eventToListenFor = RequestCurrentLocationEvent;
+
+                mapManager.attach(element);
+                mapManager.detach();
+
+                expect(subscription.dispose).toHaveBeenCalled();
+            });
+
+            it("will unsubscribe from the pageListItemSelected when detach is called", () => {
+                eventToListenFor = 'pageListItemSelected';
+
+                mapManager.attach(element);
+                mapManager.detach();
+
+                expect(subscription.dispose).toHaveBeenCalled();
+            });
+
+            it("will unsubscribe from the RequestPinDropEvent when detach is called", () => {
+                eventToListenFor = RequestPinDropEvent;
+
+                mapManager.attach(element);
+                mapManager.detach();
+
+                expect(subscription.dispose).toHaveBeenCalled();
+            });
+
+            it("will unsubscribe from the CancelPinDropEvent when detach is called", () => {
+                eventToListenFor = CancelPinDropEvent;
+
+                mapManager.attach(element);
+                mapManager.detach();
+
+                expect(subscription.dispose).toHaveBeenCalled();
+            });
+        });
+
+        describe("calling", () => {
+            let subscription: Subscription;
+
+            beforeEach(() => {
+                mapManager.attach(element);
+            });
+
+            afterEach(() => {
+                if (subscription) {
+                    subscription.dispose();
                 }
-            };
-            
-            spyOn(subscription, 'dispose').and.callThrough();
-
-            spyOn(eventAggregator, 'subscribe').and.returnValue(subscription);
-
-            mapManager.attach(element);
-            mapManager.detach();
-
-            expect(subscription.dispose).toHaveBeenCalled();
-        });
-
-        it("returns a LocationUpdateFromMapEventFactory when it receives a RequestCurrentLocationEvent", (done) => {
-            let passedCallback: Function = undefined;
-
-            spyOn(eventAggregator, 'subscribe').and.callFake((event, callback: Function) => {
-                expect(event).toEqual(RequestCurrentLocationEvent);
-                passedCallback = callback;
+                mapManager.detach();
             });
 
-            mapManager.attach(element);
+            describe("RequestCurrentLocationEvent", () => {
+                it("returns a LocationUpdateFromMapEvent when it receives a RequestCurrentLocationEvent", (done) => {
+                    subscription = eventAggregator.subscribe(LocationUpdateFromMapEvent, (response: LocationUpdateFromMapEvent) => {
+                        expect(response instanceof LocationUpdateFromMapEvent).toEqual(true);
+                        expect(response.latitude).toEqual(1);
+                        expect(response.longitude).toEqual(1);
+                        done();
+                    });
 
-            spyOn(eventAggregator, 'publish').and.callFake((payload: LocationUpdateFromMapEvent) => {
-                expect(payload instanceof LocationUpdateFromMapEvent).toEqual(true);
-                expect(payload.latitude).toEqual(mapLocation.location.latitude);
-                expect(payload.longitude).toEqual(mapLocation.location.longitude);
-                done();
+                    eventAggregator.publish(new RequestCurrentLocationEvent());
+                });
             });
 
-            // Pretend we have just received the event
-            passedCallback();
+            describe("pageListItemSelected event", () => {
+                it("enables the recenter control when a pageListItemSelected event is triggered and the control can be enabled and selected is true", () => {
+                    spyOn(recenterControl, 'enable');
+                    eventAggregator.publish('pageListItemSelected', {selected: true});
+                    expect(recenterControl.enable).toHaveBeenCalled();
+                });
+
+                it("wont enable the recenter control when a pageListItemSelected event is triggered and the control can be enabled and selected is false", () => {
+                    spyOn(recenterControl, 'enable');
+                    eventAggregator.publish('pageListItemSelected', {selected: false});
+                    expect(recenterControl.enable).not.toHaveBeenCalled();
+                });
+
+                it("wont enable the recenter control when a pageListItemSelected event is triggered and the control can not be enabled and selected is true", () => {
+                    spyOn(recenterControl, 'enable');
+                    userLocation.source = LocationSource.Map;
+                    eventAggregator.publish('pageListItemSelected', {selected: true});
+                    expect(recenterControl.enable).not.toHaveBeenCalled();
+                });
+
+                it("wont enable the recenter control when a pageListItemSelected event is triggered and the control can not be enabled and selected is false", () => {
+                    spyOn(recenterControl, 'enable');
+                    userLocation.source = LocationSource.Map;
+                    eventAggregator.publish('pageListItemSelected', {selected: false});
+                    expect(recenterControl.enable).not.toHaveBeenCalled();
+                });
+            });
+
+            describe("Pin drop events", () => {
+                it("will enable the crosshairs on the map when it receives a request event", () => {
+                    spyOn(mapCore, 'setCrosshairCursor');
+
+                    eventAggregator.publish(new RequestPinDropEvent);
+
+                    expect(mapCore.setCrosshairCursor).toHaveBeenCalled();
+                });
+
+                it("will disable the crosshairs on the map when it receives a cancel event", () => {
+                    spyOn(mapCore, 'unsetCrosshairCursor');
+
+                    eventAggregator.publish(new CancelPinDropEvent);
+
+                    expect(mapCore.unsetCrosshairCursor).toHaveBeenCalled();
+                });
+
+                it("will fire a LocationUpdateFromMapEvent if the map is clicked after RequestPinDrop event has been received", (done) => {
+                    subscription = eventAggregator.subscribe(LocationUpdateFromMapEvent, (response: LocationUpdateFromMapEvent) => {
+                        expect(response instanceof LocationUpdateFromMapEvent).toEqual(true);
+                        expect(response.latitude).toEqual(5);
+                        expect(response.longitude).toEqual(6);
+                        done();
+                    });
+
+                    eventAggregator.publish(new RequestPinDropEvent);
+                    mapCore.fireEvent('click', {latlng: {lat: 5, lng: 6}});
+                });
+
+                it("will not fire a LocationUpdateFromMapEvent if the map is clicked before RequestPinDrop event has been received", () => {
+                    subscription = eventAggregator.subscribe(LocationUpdateFromMapEvent, (response: LocationUpdateFromMapEvent) => {
+                        fail();
+                    });
+
+                    mapCore.fireEvent('click', {latlng: {lat: 5, lng: 6}});
+                });
+
+                it("will not fire a LocationUpdateFromMapEvent if the map is clicked after a RequestPinDrop and a CancelPinDrop event have been received", () => {
+                    subscription = eventAggregator.subscribe(LocationUpdateFromMapEvent, (response: LocationUpdateFromMapEvent) => {
+                        fail();
+                    });
+
+                    eventAggregator.publish(new RequestPinDropEvent);
+                    eventAggregator.publish(new CancelPinDropEvent);
+                    mapCore.fireEvent('click', {latlng: {lat: 5, lng: 6}});
+                });
+            });
         });
 
     });
@@ -598,6 +761,12 @@ describe("MapManager", () => {
         panTo(location: LocationInformation) {
         }
 
+        setCrosshairCursor() {
+        }
+
+        unsetCrosshairCursor() {
+        }
+
         fireEvent(event: string, payload?: any) {
             this.events.get(event)(payload);
         }
@@ -612,18 +781,5 @@ describe("MapManager", () => {
         location: LocationInformation = {latitude: 1, longitude: 1, heading: 0, accuracy: 0};
     }
 
-    beforeEach(() => {
-        mapCore = new MockMapCore() as any;
-        baseLayer = new MockMapLayer() as MapMapLayer;
-        userLocation = new MockLocationManager() as LocationManager;
-        mapLocation = new MockLocation() as CurrentMapLocation;
-        currentLocationMarker = new MockCurrentLocationMarker() as CurrentLocationMarker;
-        recenterControl = new MockRecenterControl() as RecenterControl;
-        eventAggregator = new EventAggregator();
-        locationUpdateFromMapEventFactory = (latitude: number, longitude: number) => new LocationUpdateFromMapEvent(latitude, longitude);
-
-        mapManager = new MapManager(bindingEngine, mapCore as any, baseLayer, userLocation, mapLocation, currentLocationMarker, recenterControl, eventAggregator, locationUpdateFromMapEventFactory);
-        element = document.createElement("div");
-    });
 
 });
