ccba072a3acc3c67c087f2402b503fb7d2204344
Kevin <kevin@algn.uk>
2017-04-03 09:55:24 +0100

    Tidied up the login process

--- src/components/elements/auth/google.ts
+++ src/components/elements/auth/google.ts
@@ -17,15 +17,15 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * - Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
+ *   notice, this list of conditions and the following disclaimer.
  * - Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
  * - The name of the University of Southampton nor the name of its
- *    contributors may be used to endorse or promote products derived from
- *    this software without specific prior written permission.
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
  *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS BE LIABLE FOR ANY
@@ -36,8 +36,17 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+import {autoinject} from "aurelia-framework";
+import {AuthService} from "aurelia-authentication";
 
-export class Login {
+@autoinject()
+export class googleCustomElement {
+     constructor(private authService: AuthService) {}
 
-
+     login() {
+        return this.authService.authenticate('google')
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 }
