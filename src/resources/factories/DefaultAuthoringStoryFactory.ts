5680bce843e726b69f021085136a51a61b2a3192
Kevin <kevin@algn.uk>
2017-04-12 09:53:57 +0100

    New stories are created without help text

--- src/resources/factories/DefaultAuthoringStoryFactory.ts
+++ src/resources/factories/DefaultAuthoringStoryFactory.ts
@@ -62,18 +62,9 @@ export class DefaultAuthoringStoryFactory {
             authorIds: [this.currentUser.userId],
             tags: [],
             pages: [{
-                // "id": "defaultPage",
                 "name": "Start Page",
-                "content": `Edit this page to create the first page in your story.
-
-To help get you started, it is useful to know that your story pages may or may not be pinned to a location on the map. A page pinned to a location can only be read in that location. By default, pages can only be read once, although you can change this.
-
-Pages first appear as 'loose pages'. But if you want, you can organise your pages into chapters. Pages in a chapter are only accessible when that chapter is open - so chapters are a good way of managing progression through your story.
-
-Your start page should always be a 'loose page,' so that it can be read at the beginning of the story. You may choose to have more than one possible start page.
-
-Enjoy writing your story!`,
-                "pageHint": "Read this page to begin the story",
+                "content": "",
+                "pageHint": "",
                 "allowMultipleReadings": false,
                 "finishesStory": false,
                 "unlockedByPageIds": [],
